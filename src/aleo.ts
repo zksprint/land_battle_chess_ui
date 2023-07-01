79c0603f62234500cb646633791ed1319cf1ca5d
365756568@qq.com <365756568@qq.com>
2023-07-02 07:59:20 +0800

    调试修改
    Signed-off-by: 365756568@qq.com <365756568@qq.com>

--- src/aleo.ts
+++ src/aleo.ts
@@ -1,10 +1,11 @@
-import { AleoNetworkClient, DevelopmentClient, Output } from "@aleohq/sdk";
+import { AleoNetworkClient, DevelopmentClient } from "@aleohq/sdk";
 import { gameId, sleep } from "./login";
 import { Game } from "./game";
 import { board } from "./event";
 import { Rank } from "./chess";
 import { getRevertLocation } from "./location";
-import { RecordPlaintext } from "@aleohq/wasm";
+import { RecordPlaintext } from "../aleo/wasm/pkg/aleo_wasm";
+
 // import { Transaction } from "@aleohq/sdk";
 const Long = require('long');
 
@@ -55,10 +56,15 @@ const Long = require('long');
   }
  */
 let moveInfo: any
+
+export function isError(result: any): result is Error {
+  return result instanceof Error;
+}
+
 export function updateMoveInfo(gameId: string, x: number, y: number,
-                        targetX: number, targetY: number, attackResult: number,
-                        flagX: number, flagY: number,
-                        oppFlagX: number, oppFlagY: number, gameWinner: number) {
+  targetX: number, targetY: number, attackResult: number,
+  flagX: number, flagY: number,
+  oppFlagX: number, oppFlagY: number, gameWinner: number) {
   moveInfo = `{
                 gameId:"${gameId}u64",
                 player: ${Game.getInstance(gameId).getLocalAddresses()},
@@ -76,43 +82,67 @@ export function updateMoveInfo(gameId: string, x: number, y: number,
   console.log(`updateMoveInfo result:${moveInfo}`)
 }
 
-export const aleoUrl: string = "http://149.28.212.193:3030"
-export const developUrl: string = "http://149.28.212.193:4040"
+export const aleoUrl: string = "http://127.0.0.1:3030"
+export const developUrl: string = "http://127.0.0.1:4040"
 export let nodeConnection: AleoNetworkClient
 export let developerClient: DevelopmentClient
 let transactionId: string = ""
 const programId = "land_battle_chess.aleo"
 
-export function newAleoClient(url: string,devUrl:string=developUrl) {
+export async function getFirstUnspentRecord(privateKey: string): Promise<RecordPlaintext | undefined> {
+  let res = await nodeConnection.getLatestHeight()
+  if (isError(res)) {
+    alert("Error fetching latest block error:.");
+    return
+  }
+  const lastHeight = res as number
+  console.log("lastHeight:", lastHeight," privateKey:",privateKey," begin to find unspend record")
+
+  const response1 = await nodeConnection.findUnspentRecords(lastHeight - 3000, lastHeight, privateKey, undefined, undefined)
+  if (isError(response1)) {
+    alert("Error fetching latest block error:.");
+    return
+  }
+  let texts = response1 as Array<RecordPlaintext>
+  for (const text of texts) {
+    if (text.microcredits() > BigInt(1000000)) {
+      console.log(`find record ${text.toString()}`)
+      return text
+    }
+  }
+  return undefined
+}
+
+export function newAleoClient(url: string, devUrl: string = developUrl) {
   console.log("init newAleoClinet....")
   nodeConnection = new AleoNetworkClient(url)
   developerClient = new DevelopmentClient(devUrl)
 }
 
-function getInitLinePiece(): [string[], string, string] {
-  let lines = [Long.UZERO, Long.UZERO, Long.UZERO, Long.UZERO, Long.UZERO];
+function getInitLinePiece(): [ string[], string, string ] {
+  let lines = [ Long.UZERO, Long.UZERO, Long.UZERO, Long.UZERO, Long.UZERO ];
   let flagXStr = "0";
   let flagYStr = "0";
 
   for (let x = 0; x < 5; x++) {
     for (let y = 0; y < 11; y++) {
-      let [revX, revY] = Game.getInstance(gameId).isPlayer1() ? [x, y] : getRevertLocation(x, y);
+      let [ revX, revY ] = Game.getInstance(gameId).isPlayer1() ? [ x, y ] : getRevertLocation(x, y);
       const chess = board.getLocationInstance(revX, revY).getChess();
       if (chess == null) {
-        lines[x] = lines[x].or(Long.fromNumber(0).shiftLeft(5 * x));
+        lines[ x ] = lines[ x ].or(Long.fromNumber(0).shiftLeft(5 * x));
         continue;
       }
 
       if (chess.rank == Rank.Flag && chess.address == Game.getInstance(gameId).getLocalAddresses()) {
         flagXStr = revX.toString() + "u64";
-        flagYStr = revY.toString() + "u64";
+        flagYStr = revY.toString() + "u32";
       }
-      lines[x] = lines[x].or(Long.fromNumber(chess.rank).shiftLeft(5 * x));
+      lines[ x ] = lines[ x ].or(Long.fromNumber(chess.rank).shiftLeft(5 * x));
     }
   }
   const newLines = lines.map(line => Long.fromNumber(line).toString() + "u64");
 
-  return [newLines, flagXStr, flagYStr];
+  return [ newLines, flagXStr, flagYStr ];
 }
 
 async function getRecordInfo(): Promise<string> {
@@ -155,23 +185,21 @@ async function getRecordInfo(): Promise<string> {
         flag_x: u64, flag_y: u32, public game_id: u64, public player_index: u32, public arbiter: address) -> (player_state, bool) 
  */
 export async function aleoInitializeBoard() {
-  // let [lines, flagXStr, flagYStr] = getInitLinePiece()
-  // const arbiter = Game.getInstance(gameId).getArbiter()
-  // const playerIndexStr = Game.getInstance(gameId).isPlayer1 ? "0u32" : "u32"
-  // console.log("begin to get unspendRecordInfo....")
-  // const recordFee = await getUnspendRecordInfo()
-  // if(recordFee.length == 0){
-  //   throw console.error("failed to find unspent records")
-  // }
+  let [ lines, flagXStr, flagYStr ] = getInitLinePiece()
+  const arbiter = Game.getInstance(gameId).getArbiter()
+  const gameIdStr = gameId+"u64"
+  const playerIndexStr = Game.getInstance(gameId).isPlayer1 ? "0u32" : "1u32"
 
   console.log(`aleoInitializeBoard`)
-  const record = getUnspendRecordInfo()
-  transactionId = await developerClient.executeProgram("hello.aleo","sum", 100,["1u32","0u32"],"APrivateKey1zkp8CZNn3yeCseEtxuVPbDCwSyhGW6yZKUYKfgXmcpoGPWH","{owner:aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px.private,  microcredits: 374989986086745u64.private,  _nonce: 2769753336736429046444578370097192476451397803312190936168551998746304500831group.public}")
-  // transactionId = await developerClient.executeProgram(programId, "player_initialize_board", 0, [lines[0], lines[1], lines[2], lines[3],
-  //                                                      lines[4], flagXStr, flagYStr, playerIndexStr, arbiter],undefined,undefined,recordFee[0].toString())
-  console.log(`aleoInitializeBoard,id:`,transactionId)
+  const privateKey = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
+  const record = await getFirstUnspentRecord(privateKey)
+  if (record == undefined){
+    throw console.error("record is not find")
+  }
 
-
+  transactionId = await developerClient.executeProgram(programId, "player_initialize_board", 10000, [ lines[ 0 ], lines[ 1 ], lines[ 2 ], lines[ 3 ],
+  lines[ 4 ], flagXStr, flagYStr, gameIdStr, playerIndexStr, arbiter ], privateKey, undefined, record.toString())
+  console.log(`aleoInitializeBoard,id:`, transactionId)
 }
 
 /**
@@ -180,11 +208,13 @@ export async function aleoInitializeBoard() {
  */
 export async function aleoMovePiece(x: number, y: number, targetX: number, targetY: number) {
   console.log(`aleoMovePiece:(x:${x}, y:${y} (targetx:${targetX}, targety:${targetY})`)
-  const playerState = await getRecordInfo()
-  console.log(`aleoMovePiece pre record info:${playerState}`)
-  transactionId = await developerClient.executeProgram(programId, "move_piece", 0, [playerState, moveInfo,
-                                                        `${x.toString()}u64`,`${y.toString()}u64`,
-                                                        `${targetX.toString()}u64`, `${targetY.toString()}u32`])
+  const record = await getRecordInfo()
+  console.log(`aleoMovePiece pre record info:${record.toString()}`)
+  const privateKey = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
+
+  transactionId = await developerClient.executeProgram(programId, "move_piece", 10000, [ moveInfo,
+    `${x.toString()}u64`, `${y.toString()}u64`,
+    `${targetX.toString()}u64`, `${targetY.toString()}u32` ], privateKey, undefined, record.toString())
 }
 
 /**
@@ -193,18 +223,9 @@ export async function aleoMovePiece(x: number, y: number, targetX: number, targe
  */
 export async function aleoWhisperPiece(targetX: number, targetY: number) {
   console.log(`aleoWhisperPiece: (targetx:${targetX}, targety:${targetY})`)
-  const playerState = await getRecordInfo()
-  console.log(`aleoWhisperPiece pre record info:${playerState}`)
-  transactionId = await developerClient.executeProgram(programId, "whisper_piece", 0, [playerState, moveInfo,
-                                                        `${targetX.toString()}u64`, `${targetY.toString()}u64`])
-}
-
-export async function getUnspendRecordInfo() {
-  const maxMicrocredits = 100000;
-  const block = await nodeConnection.getLatestHeight()
-  // const key = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
-  const key = "APrivateKey1zkp8CZNn3yeCseEtxuVPbDCwSyhGW6yZKUYKfgXmcpoGPWH"
-  let records = await nodeConnection.findUnspentRecords(0, block as number, key, undefined, maxMicrocredits);
-  console.log("finish ....")
-  return records as RecordPlaintext[]
+  const record = await getRecordInfo()
+  console.log(`aleoMovePiece pre record info:${record.toString()}`)
+  const privateKey = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
+  transactionId = await developerClient.executeProgram(programId, "whisper_piece", 0, [ moveInfo,
+    `${targetX.toString()}u64`, `${targetY.toString()}u64` ], privateKey, undefined, record.toString())
 }
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,23 @@
-import init,{ RecordCiphertext } from "@aleohq/wasm";
-import { Chess, ChessStatus, RANK_ZH,  } from "./chess";
+
+import { Chess, ChessStatus, RANK_ZH, } from "./chess";
 import { drawChess, resetChess } from "./draw";
 import { board, clearSelectChess } from "./event";
 import { EGameState, Game } from "./game";
 import { account, gameId, ws } from "./login";
-import { aleoInitializeBoard, aleoUrl, newAleoClient, nodeConnection } from "./aleo";
-import { Account } from "@aleohq/sdk";
+import { aleoInitializeBoard, aleoUrl, isError, newAleoClient, nodeConnection } from "./aleo";
+import { Block } from "@aleohq/sdk";
+import init, { RecordPlaintext } from "../aleo/wasm/pkg/aleo_wasm";
 
-init().then(async wasm=>{
+init().then(async wasm => {
   newAleoClient(aleoUrl)
-   nodeConnection.getLatestBlock().then(block => {
-    console.log("init wasm block",block)
-   })
-
-   const response = await nodeConnection.getTransaction('at1cz7alvrc664897ynxq6eyt7px4nkksm2k53n64ccx3q9fc3eyvxq4d86dp')
-   if (response instanceof Error) {
-     console.error(response);
-     return "";
-   }
-
-  // const account = new Account({privateKey:privateKey})
-  //  for (const tx of response.execution.transitions) {
-  //    // 在输出中查找满足条件的记录
-  //    const output = tx.outputs.find((output) =>
-  //      currentAccount.ownsRecordCiphertext(output.value)
-  //    );
-
-  //    if (output) {
-  //      // 解密记录文本
-  //      recordText = currentAccount.decryptRecord(output.value);
-  //      console.log(recordText);
-  //      break; // 跳出内层循环
-  //    }
-
 })
 
 
 export const CHESS_WIDTH = 60;
 export const CHESS_HEIGHT = 33;
 // export const player_color = ["red","blue"]
-export const player_color = ["#FFFFFF","#FFFFFF"]
-export const bgColors = ["#E74C3C", "#3498DB"]
+export const player_color = [ "#FFFFFF", "#FFFFFF" ]
+export const bgColors = [ "#E74C3C", "#3498DB" ]
 
 //timer
 export let timer_value = 0;
@@ -78,7 +55,7 @@ export function GameReady() {
   stopButton.style.visibility = "visible";
 
   console.log("GameReady")
-  aleoInitializeBoard().then(()=>{
+  aleoInitializeBoard().then(() => {
     ws.sendReadyEvent(gameId).then()
     clearSelectChess()
     Game.getInstance(gameId).setGameState(EGameState.WAITING_GAME_START)
@@ -98,24 +75,24 @@ export function GameStop(): void {
   gameStarted = false;
 }
 
-function isChessVisible( chess:Chess):boolean {
-	if ( chess.displayed == true || chess.address == account.toString()){
-		return true;
+function isChessVisible(chess: Chess): boolean {
+  if (chess.displayed == true || chess.address == account.toString()) {
+    return true;
   }
   return false;
 }
 
-function getPlayerIndex(address:string):number {
+function getPlayerIndex(address: string): number {
   const player1 = Game.getInstance(gameId).getPlayer1();
   return (address == player1) ? 0 : 1;
 }
 
-function getPlayerColor(playerIndex:number) {
-  return player_color[playerIndex];
+function getPlayerColor(playerIndex: number) {
+  return player_color[ playerIndex ];
 }
 
-function getBgColor(playerIndex:number) {
-  return bgColors[playerIndex]
+function getBgColor(playerIndex: number) {
+  return bgColors[ playerIndex ]
 }
 
 
@@ -128,25 +105,8 @@ export function updateDrawArray() {
       const visible = isChessVisible(chess);
       const playerIndex = getPlayerIndex(chess.address);
       const color = getPlayerColor(playerIndex);
-      const rank = RANK_ZH[chess.rank];
-      drawChess(rank, location.x, location.y, color, visible,getBgColor(playerIndex));
+      const rank = RANK_ZH[ chess.rank ];
+      drawChess(rank, location.x, location.y, color, visible, getBgColor(playerIndex));
     }
   }
 }
-
-
-
-// testAsm()
-
-// async function testAsm(){
-//     // 加载 WebAssembly 模块
-//   const wasmModule = await WebAssembly.instantiateStreaming(fetch('rust_hello.wasm'));
-
-//   // 获取 WebAssembly 函数
-//   const add_Value = wasmModule.instance.exports.add_Value as (a: number, b: number) => number;
-
-//   // 调用 WebAssembly 函数
-//   const result = add_Value(2, 3);
-
-//   console.log(result); // 输出 5
-// }
