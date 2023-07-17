864653a829dc7f74c8802ebc12abce8a36904f38
365756568@qq.com <365756568@qq.com>
2023-07-18 00:35:10 +0800

    aleo movepiece上链，现在input1解析出错
    Signed-off-by: 365756568@qq.com <365756568@qq.com>

--- src/aleo.ts
+++ src/aleo.ts
@@ -22,21 +22,21 @@ export function updateMoveInfo(gameId: string, x: number, y: number,
   targetX: number, targetY: number, attackResult: number,
   flagX: number, flagY: number,
   oppFlagX: number, oppFlagY: number, gameWinner: number) {
-  
+
   let tmpInfo = {
-      gameId:gameId+"u64",
-      player: Game.getInstance(gameId).getLocalAddresses(),
-      x:x.toString() + "u64",
-      y:y.toString() + "u32",
-      target_x: targetX.toString()+ "u64",
-      target_y: targetY.toString() + "u32",
-      attack_result: attackResult.toString() + "u32", // 0: simple move, 1: win, 2: draw, 3: lose
-      flag_x: flagX.toString()+ "u64",
-      flag_y: flagY.toString() + "u32",
-      opp_flag_x: oppFlagX.toString() + "u64",
-      opp_flag_y: oppFlagY.toString() + "u32",
-      game_winner: gameWinner.toString() + "u32"
-    }
+    gameId: gameId + "u64",
+    player: Game.getInstance(gameId).getLocalAddresses(),
+    x: x.toString() + "u64",
+    y: y.toString() + "u32",
+    target_x: targetX.toString() + "u64",
+    target_y: targetY.toString() + "u32",
+    attack_result: attackResult.toString() + "u32", // 0: simple move, 1: win, 2: draw, 3: lose
+    flag_x: flagX.toString() + "u64",
+    flag_y: flagY.toString() + "u32",
+    opp_flag_x: oppFlagX.toString() + "u64",
+    opp_flag_y: oppFlagY.toString() + "u32",
+    game_winner: gameWinner.toString() + "u32"
+  }
   moveInfo = JSON.stringify(tmpInfo)
   console.log(`updateMoveInfo result:${moveInfo}`)
 }
@@ -45,7 +45,7 @@ export const aleoUrl: string = "http://127.0.0.1:3030"
 export const developUrl: string = "http://127.0.0.1:4040"
 export let nodeConnection: AleoNetworkClient
 export let developerClient: DevelopmentClient
-let transactionId: string = ""
+let transactionId: any
 const programId = "land_battle_chess.aleo"
 
 export async function getFirstUnspentRecord(privateKey: string): Promise<RecordPlaintext | undefined> {
@@ -104,18 +104,18 @@ function getInitLinePiece(): [string[], number, number] {
   return [newLines, flagXStr, flagYStr];
 }
 
-async function getRecordInfo(txId:string,viewKey:ViewKey): Promise<RecordPlaintext[]> {
-  let recordInfo :any[]
+async function getRecordInfo(txId: string, viewKey: ViewKey): Promise<RecordPlaintext[]> {
+  let recordInfo = []
 
-  if(transactionId == ""){
+  if (transactionId == "") {
     console.log("Failed to get transaction Id")
     return
   }
 
   try {
-    const response = await nodeConnection.getTransaction(txId);
+    const response = await nodeConnection.getTransaction(txId.slice(1, -1));
     if (response instanceof Error) {
-      console.error(response);
+      console.error("!!!fetch error:" + response);
       return [];
     }
 
@@ -124,14 +124,13 @@ async function getRecordInfo(txId:string,viewKey:ViewKey): Promise<RecordPlainte
     const record = RecordCiphertext.fromString(outValue);
     const playState = record.decrypt(viewKey)
 
-    const outValue2 = txInfo.execution.fee[0].outputs[0].value
+    const outValue2 = txInfo.fee.transition.outputs[0].value
     const unspentRecord = RecordCiphertext.fromString(outValue2);
     const unspent = unspentRecord.decrypt(viewKey)
 
-    recordInfo.push(playState,unspent)
-    console.log(`---------getRecordInfo recordText:${outValue} playState:${playState} unspentRecord:${unspentRecord} text:${unspent}----------------`)
-
-
+    recordInfo.push(playState)
+    recordInfo.push(unspent)
+    console.log(`---------getRecordInfo playState:${playState.toString()}  unspentRecord:${unspent.toString()}----------------`)
   } catch (error) {
     console.error(error);
   }
@@ -151,19 +150,11 @@ export async function aleoInitializeBoard() {
 
   console.log(`aleoInitializeBoard`)
   const privateKey = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
-  // const record = await getFirstUnspentRecord(privateKey)
-  // if (record == undefined){
-  //   throw console.error("record is not find")
-  // }
-
-  // transactionId = await developerClient.executeProgram(programId, "player_initialize_board", 10000, [ "243944968059068u64", "243944970543153u64" ,"243944969889843u64", 
-  // "243944969494674u64", "243944969840532u64", "1u64", "0u32", "1u64", "1u32", "aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px" ], privateKey, undefined, record.toString())
-  // console.log(`aleoInitializeBoard,id:`, transactionId)
   updateMoveInfo(gameId, 0, 0, 0, 0, 0, flagXStr, flagYStr, 0, 0, 0)
 
   transactionId = await developerClient.executeProgram(programId, "player_initialize_board", 1, [lines[0], lines[1], lines[2], lines[3],
   lines[4], flagXStr.toString() + "u64", flagYStr.toString() + "u32", gameIdStr, playerIndexStr, arbiter], privateKey, undefined)
-  console.log(`aleoInitializeBoard,id:`, transactionId)
+  console.log(`aleoInitializeBoard,id:${transactionId}`)
 }
 
 /**
@@ -172,19 +163,13 @@ export async function aleoInitializeBoard() {
  */
 export async function aleoMovePiece(x: number, y: number, targetX: number, targetY: number) {
   console.log(`aleoMovePiece:(x:${x}, y:${y} (targetx:${targetX}, targety:${targetY})`)
-  // const record = await getRecordInfo()
-  // console.log(`aleoMovePiece pre record info:${record.toString()}`)
+
   const privateKey = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
+  const [playState, recordFee] = await getRecordInfo(transactionId, Game.getInstance(gameId).getCurrentAccount().viewKey())
 
-  const [playState,recordFee] = await getRecordInfo(transactionId,Game.getInstance(gameId).getCurrentAccount().viewKey())
-
-  // transactionId = await developerClient.executeProgram(programId, "move_piece", 10000, [ moveInfo,
-  //   `${x.toString()}u64`, `${y.toString()}u64`,
-  //   `${targetX.toString()}u64`, `${targetY.toString()}u32` ], privateKey, undefined, record.toString())
-
-  transactionId = await developerClient.executeProgram(programId, "move_piece", 1, [playState.toString(),moveInfo,
-    `${x.toString()}u64`, `${y.toString()}u32`,
-    `${targetX.toString()}u64`, `${targetY.toString()}u32`], privateKey, undefined,recordFee.toString())
+  transactionId = await developerClient.executeProgram(programId, "move_piece", 1, [playState.toString(), moveInfo,
+  x.toString() + "u64", y.toString() + "u32",
+  targetX.toString() + "u64", targetY.toString() + "u32"], privateKey, undefined, recordFee.toString())
 }
 
 /**
@@ -193,17 +178,15 @@ export async function aleoMovePiece(x: number, y: number, targetX: number, targe
  */
 export async function aleoWhisperPiece(targetX: number, targetY: number) {
   console.log(`aleoWhisperPiece: (targetx:${targetX}, targety:${targetY})`)
-  const [playState,recordFee] = await getRecordInfo(transactionId,Game.getInstance(gameId).getCurrentAccount().viewKey())
+  const [playState, recordFee] = await getRecordInfo(transactionId, Game.getInstance(gameId).getCurrentAccount().viewKey())
 
   const privateKey = Game.getInstance(gameId).getCurrentAccount().privateKey().to_string()
-  // transactionId = await developerClient.executeProgram(programId, "whisper_piece", 0, [moveInfo,
-  //   `${targetX.toString()}u64`, `${targetY.toString()}u32`], privateKey, undefined, record.toString())
 
   transactionId = await developerClient.executeProgram(programId, "whisper_piece", 1, [playState.toString(), moveInfo,
-    `${targetX.toString()}u64`, `${targetY.toString()}u32`], privateKey, undefined, recordFee.toString())
+    targetX.toString()+ "u64", targetY.toString() + "u32"], privateKey, undefined, recordFee.toString())
 
-  const tx = await nodeConnection.getTransaction(transactionId)  
-  if(isError(tx)){
+  const tx = await nodeConnection.getTransaction(transactionId)
+  if (isError(tx)) {
     console.log(`Failed to whisperPiece id:${gameId} localAddress:${Game.getInstance(gameId).getLocalAddresses()}`)
     return ""
   }
